a0ccc14872d49de9471af5eae6c666309c61d1c0
csagymbek <csagymbek@gmail.com>
2022-04-16 00:41:37 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Task Manager App')
+const express = require("express");
+const tasks = require("./routes/tasks");
+
+const app = express();
+const port = 3000;
+
+// middleware
+app.use(express.json());
+
+// routes
+app.use("/api/v1/tasks", tasks);
+
+// listening
+app.listen(port, console.log(`listening on port ${port}`));
---
+++ controllers/tasks.js
@@ -0,0 +1,26 @@
+const getAllTasks = (req, res) => {
+  res.send("get all tasks");
+};
+
+const getTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+
+const createTask = (req, res) => {
+  res.json(req.body);
+};
+
+const updateTask = (req, res) => {
+  res.json(req.body);
+};
+const deleteTask = (req, res) => {
+  res.send("delete task");
+};
+
+module.exports = {
+  getAllTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
+};
